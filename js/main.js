46df169d5f931ea15fe30b86f90b5058dc447d1c
muskan3001 <mg8203@srmist.edu.in>
2021-08-10 12:42:31 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -215,6 +215,9 @@ function bodyScrollingToggle() {
       ".portfolio-item-title"
     ).innerHTML;
     popup.querySelector(".pp-title h2").innerHTML = title;
+
+    popup.querySelector(".pp-project-details").innerHTML = details;
+
     const category = portfolioItems[itemIndex].getAttribute("data-category");
     popup.querySelector(".pp-project-category").innerHTML = category
       .split("-")
@@ -242,14 +245,14 @@ function bodyScrollingToggle() {
 })();
 
 // Hide all section except current
-(() => {
-  const sections = document.querySelectorAll(".section");
-  sections.forEach((section) => {
-    if (!section.classList.contains("active")) {
-      section.classList.add("hide");
-    }
-  });
-})();
+// (() => {
+//   const sections = document.querySelectorAll(".section");
+//   sections.forEach((section) => {
+//     if (!section.classList.contains("active")) {
+//       section.classList.add("hide");
+//     }
+//   });
+// })();
 
 const loader = document.querySelector(".loader");
 const body1 = document.querySelector("body");
@@ -267,5 +270,5 @@ window.addEventListener("load", () => {
     header.style.visibility = "visible";
     header1.style.visibility = "visible";
     styleSwitcher1.style.visibility = "visible";
-  }, 3500);
+  }, 100);
 });
